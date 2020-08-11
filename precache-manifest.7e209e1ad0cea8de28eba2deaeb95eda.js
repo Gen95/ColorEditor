2d4c735f6f983ba256186ba39bff94649886dcba
Gen95 <varg.warder@gmail.com>
2020-08-11 13:22:02 +0300

    Updates

---
+++ precache-manifest.7e209e1ad0cea8de28eba2deaeb95eda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99ad0f55bcc7056f22bee7ad1750e91",
+    "url": "/ColorEditor/index.html"
+  },
+  {
+    "revision": "34ad2503106c5d3fd2b2",
+    "url": "/ColorEditor/static/css/main.8620df82.chunk.css"
+  },
+  {
+    "revision": "0adc0abcdaded9b78856",
+    "url": "/ColorEditor/static/js/2.0cc0bab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ColorEditor/static/js/2.0cc0bab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ad2503106c5d3fd2b2",
+    "url": "/ColorEditor/static/js/main.0e28035d.chunk.js"
+  },
+  {
+    "revision": "c403c01e10b38a1fecd5",
+    "url": "/ColorEditor/static/js/runtime-main.09abe6e9.js"
+  }
+]);
